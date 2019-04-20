402e8d5191f483ccb39ba4c9e5ee0224820f0c02
pikacsc <ccc11203@gmail.com>
2019-04-21 00:35:24 +0900

    [ success exchange to cpp based BP_BattleRoyalCharacter liks BP_PlayerBase ]

--- Source/BattleRoyal/BattleRoyalCharacter.h
+++ Source/BattleRoyal/BattleRoyalCharacter.h
@@ -87,6 +87,7 @@ protected:
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
 	void LookUpAtRate(float Rate);
+
 protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
